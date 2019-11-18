66647701177e08c2de336da6b1cd307e40ac31cb
Marcos Domingues <m.domingues@tjrs.gov.br>
2019-11-18 09:24:03 -0300

    Submitting a hello word with primitive types on Eclipse.

---
+++ java-helloword/src/alura/curso/java/basico/TesteTiposPrimitivos.java
@@ -0,0 +1,20 @@
+package alura.curso.java.basico;
+
+public class TesteTiposPrimitivos {
+
+	public static void main(String[] args) {
+		System.out.println("Test of primitive types.");
+		System.out.println();
+
+		String name = "Marcos Domingues";
+		int age = 37;
+		double costOfCourse = 100.00;
+		char active = 'A';
+
+		System.out.println("Student name: " + name);
+		System.out.println("Age:" + age);
+		System.out.println("Cost of course: $" + costOfCourse);
+		System.out.println("Situation (Ativo=A): " + active);
+	}
+
+}
