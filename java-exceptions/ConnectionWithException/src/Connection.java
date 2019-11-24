1a255fa52fe201a762eba047159816cd6cc87891
Marcos Domingues <mdomingues.ti@gmail.com>
2019-11-24 10:03:27 -0200

    Project with AutoCloseable block (try/finally).

---
+++ java-exceptions/ConnectionWithException/src/Connection.java
@@ -0,0 +1,18 @@
+
+public class Connection implements AutoCloseable {
+
+	public void open() {
+		System.out.println("Connection was opened.");
+	}
+
+	public void readData() {
+		System.out.println("Reading data from connection.");
+		throw new RuntimeException("Server is down.");
+	}
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("Connection was closed.");
+		
+	}
+}
