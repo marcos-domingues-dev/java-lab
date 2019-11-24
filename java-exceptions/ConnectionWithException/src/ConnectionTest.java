46c85eb6e873f0e317f5aa7b9f8d91d105753ad5
Marcos Domingues <mdomingues.ti@gmail.com>
2019-11-24 10:03:27 -0200

    Project with AutoCloseable block (try/finally).

---
+++ java-exceptions/ConnectionWithException/src/ConnectionTest.java
@@ -0,0 +1,18 @@
+
+public class ConnectionTest {
+
+	public static void main(String[] args) {
+
+		// This class "Connection" implements a AutoCloseable 
+		// interface, and alwais will do the finnaly block
+		// calling the "close()" method.
+		
+		try (Connection connection = new Connection()) {
+			connection.open();
+			connection.readData();
+			connection.close();
+		} catch (Exception ex) {
+			System.out.println("An error was ocurred." + ex.getMessage());
+		}
+	}
+}
