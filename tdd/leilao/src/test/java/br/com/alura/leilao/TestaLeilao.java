915c29ce88de1712a6a6414bca6a5701ce7fb301
Marcos Domingues <m.domingues@tjrs.gov.br>
2020-02-10 08:41:10 -0300

    TDD - @Before e Test Class Builder

--- tdd/leilao/src/test/java/br/com/alura/leilao/TestaLeilao.java
+++ tdd/leilao/src/test/java/br/com/alura/leilao/TestaLeilao.java
@@ -2,85 +2,61 @@ package br.com.alura.leilao;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestaLeilao {
+    private Usuario steveJobs;
+    private Usuario billGates;
+    private Usuario magnata;
+
+    @Before
+    public void setup() {
+        this.steveJobs = new Usuario("Steve Jobs");
+        this.billGates = new Usuario("Bill Gates");
+         this.magnata = new Usuario("Magnata do Petroleo");
+    }
 
     @Test
     public void deveReceber1Lance() {
-        // given
-        Usuario steveJobs = new Usuario("Steve Jobs");
-        Leilao leilao = new Leilao("Macbook Pro 15");
+        final Leilao leilao = new LeilaoBuilder().para("Macbook Pro 15").lance(steveJobs, 2000).pegar();
 
-        // when
-        leilao.propoe(new Lance(steveJobs, 2000));
-
-        // then
-        int quantidadeEsperada = 1;
+        final int quantidadeEsperada = 1;
         assertEquals(quantidadeEsperada, leilao.getLances().size());
     }
 
     @Test
     public void naoDeveAceitarMaisDoQue5LanesDeUmMesmoUsuario() {
-        // given
-        Usuario steveJobs = new Usuario("Steve Jobs");
-        Usuario billGates = new Usuario("Bill Gates");
-        Leilao leilao = new Leilao("Macbook Pro 15");
-
-        // when
-        leilao.propoe(new Lance(steveJobs, 2000));
-        leilao.propoe(new Lance(billGates, 2500));
-        
-        leilao.propoe(new Lance(steveJobs, 3000));
-        leilao.propoe(new Lance(billGates, 3500));
-
-        leilao.propoe(new Lance(steveJobs, 4000));
-        leilao.propoe(new Lance(billGates, 4500));
-
-        leilao.propoe(new Lance(steveJobs, 5000));
-        leilao.propoe(new Lance(billGates, 5500));
+        final Leilao leilao = new LeilaoBuilder().para("Macbook Pro 15").lance(steveJobs, 2000).lance(billGates, 2500)
+                .lance(steveJobs, 3000).lance(billGates, 3500).lance(steveJobs, 4000).lance(billGates, 4500)
+                .lance(steveJobs, 5000).lance(billGates, 5500).lance(steveJobs, 6000).lance(billGates, 6500).pegar();
 
         leilao.propoe(new Lance(steveJobs, 6000));
-        leilao.propoe(new Lance(billGates, 6500));
 
-        // deve ser ignorado
-        leilao.propoe(new Lance(steveJobs, 6000));
-        
-        // then
-        int quantidadeEsperada = 10;
+        final int quantidadeEsperada = 10;
         assertEquals(quantidadeEsperada, leilao.getLances().size());
     }
 
     @Test
     public void deveDobrarOLanceDoUsuario() {
-        // guiven
-        Usuario magnata = new Usuario("Magnata do Petroleo");
-        Usuario billGates = new Usuario("Bill Gates");
-        Leilao leilao = new Leilao("Terreno no estado do Texas.");
-
-        // when
-        leilao.propoe(new Lance(magnata, 100));
-        leilao.propoe(new Lance(billGates, 150));        
+        final Leilao leilao = new LeilaoBuilder().para("Terreno no estado do Texas.").lance(magnata, 100)
+            .lance(magnata, 100)
+            .lance(billGates, 150)
+            .pegar();
         leilao.dobrarUltimoLance(magnata);
 
-        // then
-        double valorEsperadoEmDobro = 200;
+        final double valorEsperadoEmDobro = 200;
         assertEquals(valorEsperadoEmDobro, leilao.getValorUltimoLanceDo(magnata), 0.00001);
     }
 
     @Test
     public void naoDeveDobrarCasoNaoTenhaLanceAnteriorDoUsuario() {
-        // guiven
-        Usuario magnata = new Usuario("Magnata do Petroleo");
-        Usuario billGates = new Usuario("Bill Gates");
-        Leilao leilao = new Leilao("Terreno no estado do Texas.");
-
-        // when
-        leilao.propoe(new Lance(billGates, 150));        
+        final Leilao leilao = new LeilaoBuilder().para("Terreno no estado do Texas.")
+            .lance(billGates, 150)
+            .pegar();
         leilao.dobrarUltimoLance(magnata);
 
-        // then
-        double valorEsperadoEmDobro = 0;
+        final double valorEsperadoEmDobro = 0;
         assertEquals(valorEsperadoEmDobro, leilao.getValorUltimoLanceDo(magnata), 0.00001);
     }
 }
