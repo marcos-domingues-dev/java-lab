73332fb162a28afb24fb6595249571c5b907f8d8
Marcos Domingues <mdomingues.ti@gmail.com>
2020-03-15 07:32:20 -0300

    Java: Servlet - Fazendo o Servlet ser criado ao subir o servidor.

--- gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -8,11 +8,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+@WebServlet(urlPatterns = "/oi", loadOnStartup=1)
 public class OiMundoServlet extends HttpServlet {
-
+	
 	private static final long serialVersionUID = -4628190983452256105L;
 
+	public OiMundoServlet() {
+		System.out.println("Criando OiMundoServlet");
+	}
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		PrintWriter writer = resp.getWriter();
